b41efc3d2e6f756784ec26704b8ac045c9bfef1c
pragati-patil05 <praggupatil@gmail.com>
2026-03-09 23:59:29 -0700

    Update orderRoutes.js

--- E-commerceapp/backend/routes/orderRoutes.js
+++ E-commerceapp/backend/routes/orderRoutes.js
@@ -5,16 +5,13 @@ const nodemailer = require("nodemailer");
 router.post("/create-order", async (req, res) => {
   try {
     const { user, cartItems, total } = req.body;
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     const itemList = cartItems
       .map((item) => `${item.name} - ₹${item.price}`)
@@ -49,4 +46,5 @@ Thank you for shopping with us!
 });
 
 module.exports = router;
+
 
